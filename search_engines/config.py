42840be59ffa035fd6bd5542b5afd2f778b77397
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2018-12-31 01:37:16 +0200

    Add files via upload

---
+++ search_engines/config.py
@@ -0,0 +1,31 @@
+from os import path, pardir, name as os_name
+from sys import version_info
+
+
+## Python version 
+python_version = version_info.major
+
+## Maximum number or pages 
+max_pages = 10
+
+## Domains in this list will not be collected 
+blacklist = []
+
+## HTTP request timeout 
+timeout = 10
+
+## User-Agent string 
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:51.0) Gecko/20100101 Firefox/51.0'
+
+## Proxy address 
+proxy = None
+
+## TOR proxy address 
+tor = 'socks5h://127.0.0.1:9150'
+
+base_dir = path.abspath(path.dirname(path.abspath(__file__)))
+
+## Path to report files 
+report_files_dir = path.join(base_dir, 'reports') + path.sep
+
+
