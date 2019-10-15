1e5be45056a6c040d374c3d78c1d3c4f9fada1c1
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2019-10-15 12:25:56 +0300

    Update config.py

--- search_engines/config.py
+++ search_engines/config.py
@@ -1,15 +1,12 @@
-from os import path, pardir, name as os_name
+from os import path as os_path, pardir as os_pardir, name as os_name
 from sys import version_info
 
 
 ## Python version 
 python_version = version_info.major
 
-## Maximum number or pages 
-max_pages = 10
-
-## Domains in this list will not be collected 
-blacklist = []
+## Maximum number or pages to search
+search_pages = 10
 
 ## HTTP request timeout 
 timeout = 10
@@ -21,11 +18,10 @@ user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:51.0) Gecko/20100101 Firefox/51.0'
 proxy = None
 
 ## TOR proxy address 
-tor = 'socks5h://127.0.0.1:9150'
+tor = 'socks5h://127.0.0.1:9050'
 
-base_dir = path.abspath(path.dirname(path.abspath(__file__)))
+base_dir = os_path.abspath(os_path.dirname(os_path.abspath(__file__)))
 
 ## Path to report files 
-report_files_dir = path.join(base_dir, 'reports') + path.sep
-
+report_files_dir = os_path.join(base_dir, 'reports') + os_path.sep
 
