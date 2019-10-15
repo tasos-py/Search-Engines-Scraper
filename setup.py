ccca15b0be8c182c46e51e3a0b83684e26e70626
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2019-10-15 12:31:21 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+setup(
+    name='search_engines',
+    version='0.4',
+    description='Search Engines Scraper',
+    author='Tasos M. Adamopoulos',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=requirements
+)
