859c363517ac74aaf6dc042b5cf0df8cd03e5a39
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2020-02-14 00:20:39 +0200

    Add files via upload

---
+++ search_engines/engines/__init__.py
@@ -0,0 +1,25 @@
+from .aol import Aol
+from .ask import Ask
+from .bing import Bing
+from .dogpile import Dogpile
+from .duckduckgo import Duckduckgo
+from .google import Google
+from .mojeek import Mojeek
+from .startpage import Startpage
+from .torch import Torch
+from .yahoo import Yahoo
+
+
+search_engines_dict = { 
+    'google': Google, 
+    'bing': Bing, 
+    'yahoo': Yahoo, 
+    'aol': Aol, 
+    'duckduckgo': Duckduckgo, 
+    'startpage': Startpage, 
+    'dogpile': Dogpile, 
+    'ask': Ask, 
+    'mojeek': Mojeek, 
+    'torch': Torch 
+}
+
