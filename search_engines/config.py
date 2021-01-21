0ad59d43d1ba68fb805310d24e2ef4cdab1d63ba
Sean Bailey <seanbailey518@gmail.com>
2021-01-21 12:41:59 -0500

    Provided a random useragent for improved success for searching

--- search_engines/config.py
+++ search_engines/config.py
@@ -1,5 +1,6 @@
 from os import path as os_path, pardir as os_pardir, name as os_name
 from sys import version_info
+from fake_useragent import UserAgent
 
 
 ## Python version 
@@ -12,16 +13,16 @@ SEARCH_ENGINE_RESULTS_PAGES = 20
 TIMEOUT = 10
 
 ## Default User-Agent string 
-USER_AGENT = 'search_engines/0.5 Repo: https://github.com/tasos-py/Search-Engines-Scraper'
+USER_AGENT = str(UserAgent().random)
 
 ## Fake User-Agent string - Google desn't like the default user-agent
-FAKE_USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; rv:73.0) Gecko/20100101 Firefox/73.0'
+FAKE_USER_AGENT = str(UserAgent().random)
 
 ## Proxy server 
 PROXY = None
 
 ## TOR proxy server 
-TOR = 'socks5h://127.0.0.1:9050'
+TOR = 'socks5h://127.0.0.1:1050'
 
 _base_dir = os_path.abspath(os_path.dirname(os_path.abspath(__file__)))
 
