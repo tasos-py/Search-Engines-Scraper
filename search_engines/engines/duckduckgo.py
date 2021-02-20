96a583260ee5ac813b641c194c10d3ba54a00c21
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2021-02-20 04:15:17 +0200

    Update duckduckgo.py

--- search_engines/engines/duckduckgo.py
+++ search_engines/engines/duckduckgo.py
@@ -6,7 +6,7 @@ class Duckduckgo(SearchEngine):
     '''Searches duckduckgo.com'''
     def __init__(self, proxy=PROXY, timeout=TIMEOUT):
         super(Duckduckgo, self).__init__(proxy, timeout)
-        self._base_url = 'https://duckduckgo.com/html/'
+        self._base_url = 'https://html.duckduckgo.com/html/'
     
     def _selectors(self, element):
         '''Returns the appropriate CSS selector.'''
@@ -15,7 +15,7 @@ class Duckduckgo(SearchEngine):
             'title': 'h2.result__title a', 
             'text': 'a.result__snippet', 
             'links': 'div.results div.result.results_links.results_links_deep.web-result', 
-            'next': 'div.nav-link form input[name]'
+            'next': {'forms':'div.nav-link > form', 'inputs':'input[name]'}
         }
         return selectors[element]
     
@@ -26,10 +26,12 @@ class Duckduckgo(SearchEngine):
     
     def _next_page(self, tags):
         '''Returns the next page URL and post data (if any)'''
-        inputs = tags.select(self._selectors('next'))
+        selector = self._selectors('next')
+        forms = tags.select(selector['forms'])
         url, data = None, None
-        if inputs:
-            data = {i['name']:i.get('value', '') for i in inputs}
+
+        if forms:
+            form = forms[-1]
+            data = {i['name']:i.get('value', '') for i in form.select(selector['inputs'])}
             url = self._base_url
         return {'url':url, 'data':data}
-
