17f5c4e13ef8f1d2ba0d832dcd775343f4a1e29d
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2022-04-29 11:17:31 +0300

    Update engine.py

--- search_engines/engine.py
+++ search_engines/engine.py
@@ -154,6 +154,7 @@ class SearchEngine(object):
         '''
         out.console('Searching {}'.format(self.__class__.__name__))
         self._query = utils.decode_bytes(query)
+        self.results = SearchResults()
         request = self._first_page()
 
         for page in range(1, pages + 1):
