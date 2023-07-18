1ae9e87bf91d33a9c98bd4c167890b8ce2d7e358
Tasos M Adamopoulos <35203332+tasos-py@users.noreply.github.com>
2023-07-18 09:02:10 +0300

    Fix #59 with the generous contribution of @sandervdamme

--- search_engines/engines/duckduckgo.py
+++ search_engines/engines/duckduckgo.py
@@ -1,67 +1,46 @@
 from ..engine import SearchEngine
-from ..config import PROXY, TIMEOUT
-
-import re
-import json
-from bs4 import BeautifulSoup
-
+from ..config import PROXY, TIMEOUT, FAKE_USER_AGENT
+from ..utils import unquote_url, quote_url
 
 class Duckduckgo(SearchEngine):
     '''Searches duckduckgo.com'''
     def __init__(self, proxy=PROXY, timeout=TIMEOUT):
         super(Duckduckgo, self).__init__(proxy, timeout)
-        self._base_url = u'https://links.duckduckgo.com{}&biaexp=b&msvrtexp=b&videxp=a&nadse=b&tjsexp=b'
-        self._main_url = u'https://duckduckgo.com/?q={}&t=h_'
-        self._current_page = None
+        self._base_url = u'https://html.duckduckgo.com'
+        self._current_page = 1
+        self.set_headers({'User-Agent':FAKE_USER_AGENT})
 
     def _selectors(self, element):
-        '''Returns the appropriate CSS selector - regex pattern, in this case.'''
+        '''Returns the appropriate CSS selector.'''
         selectors = {
-            'first_page': r'DDG\.deep\.initialize\(\'(.*?)\'\)', 
-            'next_page': r'"n"\:\s*"(/d\.js.*?)"', 
-            'results': r"DDG\.pageLayout\.load\('d'\,\s*(\[.*?\])\s*\);"
+            'url': 'a.result__a', 
+            'title': 'a.result__a', 
+            'text': 'a.result__snippet',
+            'links': 'div#links div.result',
+            'next': 'input[value="next"]'
         }
         return selectors[element]
     
     def _first_page(self):
         '''Returns the initial page and query.'''
-        res = self._http_client.get(self._main_url.format(self._query))
-        match = re.search(self._selectors('first_page'), res.html)
-        if match:
-            return {'url':self._base_url.format(match.group(1)), 'data':None}
-        return {'url':None, 'data':None}
+        url = u'{}/html/?q={}'.format(self._base_url, quote_url(self._query, ''))
+        return {'url':url, 'data':None}
     
     def _next_page(self, tags):
         '''Returns the next page URL and post data (if any)'''
-        match = re.search(self._selectors('next_page'), tags.get_text())
-        if match:
-            return {'url':self._base_url.format(match.group(1)), 'data':None}
-        return {'url':None, 'data':None}
+        self._current_page += 1
+        selector = self._selectors('next').format(page=self._current_page)
+        next_page = self._get_tag_item(tags.select_one(selector), 'href')
+        url = None
+        if next_page:
+            url = self._base_url + next_page
+        return {'url':url, 'data':None}
 
-    def _get_page(self, page, data=None):
-        '''Gets pagination links.'''
-        self._http_client.session.headers['Referer'] = 'https://duckduckgo.com/'
-        response = self._http_client.get(page)
-        self._current_page = response.html
-        return response
+    def _get_url(self, tag, item='href'):
+        '''Returns the URL of search results item.'''
+        selector = self._selectors('url')
+        url = self._get_tag_item(tag.select_one(selector), item)
 
-    def _filter_results(self, soup):
-        '''Processes and filters the search results.''' 
-        match = re.search(self._selectors('results'), self._current_page)
-        if not match:
-            return {}
-        data = json.loads(re.sub('\n|\r', '', match.group(1)))[:-1]
-        results = [
-            {'link':i['u'], 'title':i['t'], 'text': BeautifulSoup(i['a'], 'html.parser').get_text()} 
-            for i in data
-        ]
-
-        if u'url' in self._filters:
-            results = [l for l in results if self._query_in(l['link'])]
-        if u'title' in self._filters:
-            results = [l for l in results if self._query_in(l['title'])]
-        if u'text' in self._filters:
-            results = [l for l in results if self._query_in(l['text'])]
-        if u'host' in self._filters:
-            results = [l for l in results if self._query_in(utils.domain(l['link']))]
-        return results
+        if url.startswith(u'/url?q='):
+            url = url.replace(u'/url?q=', u'').split(u'&sa=')[0]
+        return unquote_url(url)
